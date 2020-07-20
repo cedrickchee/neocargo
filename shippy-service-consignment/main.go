09b9a580803c6dc28c12d84a190b29cd00efd59b
Cedric Chee <cedric@invictusbyte.com>
2020-07-20 13:55:36 +0000

    Tidy up
    * Organize Go codebase
    * Refactor main program

--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	// Import the generated protobuf code
 	pb "github.com/haxorbit/shippy/shippy-service-consignment/proto/consignment"
@@ -11,83 +12,13 @@ import (
 	"github.com/micro/go-micro/v2"
 )
 
-type repository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
-
-// Repository - Dummy repository, this simulates the use of a datastore
-// of some kind. We'll replace this with a real implementation later on.
-type Repository struct {
-	consignments []*pb.Consignment
-}
-
-// Create a new consignment
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	return consignment, nil
-}
-
-// GetAll consignments
-func (repo *Repository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
-// Service should implement all of the methods to satisfy the service
-// we defined in our protobuf definition. You can check the interface
-// in the generated code itself for the exact method signatures etc
-// to give you a better idea.
-type consignmentService struct {
-	repo         repository
-	vesselClient vesselProto.VesselService
-}
-
-// CreateConsignment - we created just one method on our service,
-// which is a create method, which takes a context and a request as an
-// argument, these are handled by the gRPC server.
-func (s *consignmentService) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-
-	// Here we call a client instance of our vessel service with our consignment weight,
-	// and the amount of containers as the capacity value
-	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
-		MaxWeight: req.Weight,
-		Capacity:  int32(len(req.Containers)),
-	})
-	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
-	if err != nil {
-		return err
-	}
-
-	// We set the VesselId as the vessel we got back from our
-	// vessel service
-	req.VesselId = vesselResponse.Vessel.Id
-
-	// Save our consignment
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
-
-	// Return matching the `Response` message we created in our
-	// protobuf definition.
-	res.Created = true
-	res.Consignment = consignment
-	return nil
-}
-
-// GetConsignments
-func (s *consignmentService) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
-}
+const (
+	defaultHost = "datastore:27017"
+)
 
 func main() {
 
-	repo := &Repository{}
-
-	// Create a new service. Optionally include some options here.
+	// Set up micro instance
 	service := micro.NewService(
 
 		// This name must match the package name given in your protobuf definition
@@ -97,13 +28,28 @@ func main() {
 	// Init will parse the command line flags.
 	service.Init()
 
-	// vesselService := micro.NewService(micro.Name("shippy.service.vessel"))
-	// vesselService.Init()
-	// vesselClient := vesselProto.NewVesselService("shippy.service.vessel", vesselService.Client())
+	uri := os.Getenv("DB_HOST")
+	if uri == "" {
+		uri = defaultHost
+	}
+
+	// Create database client
+	client, err := CreateClient(context.Background(), uri, 0)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer client.Disconnect(context.Background())
+
+	// Initialize database and collection
+	consignmentCollection := client.Database("shippy").Collection("consignments")
+	repository := &MongoRepository{consignmentCollection}
+
 	vesselClient := vesselProto.NewVesselService("shippy.service.vessel", service.Client())
 
-	// Register service
-	if err := pb.RegisterShippingServiceHandler(service.Server(), &consignmentService{repo, vesselClient}); err != nil {
+	h := &handler{repository, vesselClient}
+
+	// Register handlers
+	if err := pb.RegisterShippingServiceHandler(service.Server(), h); err != nil {
 		log.Panic(err)
 	}
 
