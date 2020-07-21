aa7a2a6d7c54712a51df568f981b6ddc0144df02
Cedric Chee <cedric@invictusbyte.com>
2020-07-21 13:28:22 +0000

    Add log for debugging

--- shippy-service-user/database.go
+++ shippy-service-user/database.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/jmoiron/sqlx"
@@ -12,15 +13,23 @@ import (
 
 // NewConnection returns a new database connection instance.
 func NewConnection() (*sqlx.DB, error) {
+	log.Printf("DB_HOST: %v\n", os.Getenv("DB_HOST"))
+
 	host := os.Getenv("DB_HOST")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 	conn := fmt.Sprintf("host=%s user=%s dbname=%s password=%s sslmode=disable", host, user, dbName, password)
+	log.Printf("conn: %v\n", conn)
+
 	db, err := sqlx.Connect("postgres", conn)
+	log.Println("sqlx connect done")
+
 	if err != nil {
+		log.Printf("sqlx connect err: %v\n", err)
 		return nil, err
 	}
 
+	log.Printf("DB connection OK: %v\n", db)
 	return db, nil
 }
